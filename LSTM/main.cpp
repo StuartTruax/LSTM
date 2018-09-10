3b3bc09d34afe5e02b74ec3a67acdf9f61f44ccb
Stuart Truax <stuarttruax@Stuarts-MacBook-Pro.local>
2018-09-10 10:21:13 -0700

    Initial Commit

---
+++ LSTM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LSTM
+//
+//  Created by Stuart Truax on 9/10/18.
+//  Copyright © 2018 Stuart Truax. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
